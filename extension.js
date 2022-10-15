0ccba6f2e8fb380fdb329addbab9b6aeb9e4f6f8
DadonStyle <noamoni9@gmail.com>
2022-10-16 01:24:49 +0300

    add seperation between functions

--- extension.js
+++ extension.js
@@ -1,52 +1,103 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/* imports */
 const vscode = require('vscode');
 const fs = require('fs');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+/* vars */
+const wsedit = new vscode.WorkspaceEdit();
+const fileNameString = 'styled.js'
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	console.log('start');
-	let disposable = vscode.commands.registerCommand('react-style-component-helper.helloWorld', async function () {
-		const wsedit = new vscode.WorkspaceEdit();
+	
+	let disposable = vscode.commands.registerCommand('react-style-component-helper.helloWorld', async function () { // this syntax is a template from vscode
+		/* varbs */
 		const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath; // gets the path of the first workspace folder
-		const filePath = vscode.Uri.file(wsPath + '/styled.js');
-		// if (fs.existsSync(wsPath + '/styled.js')) {
-		// 	vscode.window.showInformationMessage('Your file already created!');
-		// 	// wsedit.deleteFile(filePath);
-		// }
-		wsedit.createFile(filePath, { overwrite: false });
-		await vscode.workspace.applyEdit(wsedit);
+		let tagsArray = [];
+
+		/* validate that the user have any file open while executing the command */
 		if (typeof vscode.window.activeTextEditor === typeof undefined) {
-			 return vscode.window.showInformationMessage('Please enter the required file and try again');
+			return vscode.window.showInformationMessage('Please enter the required file and try again');
 		}
-		const contents = fs.readFileSync(vscode.window.activeTextEditor.document.fileName, 'utf-8');
-		const arrFile = contents.split(/\r?\n/);
-		console.log('Im the file name', vscode.window.activeTextEditor.document.fileName);
-		console.log('File Array', arrFile[1]);
-		vscode.window.showInformationMessage('Your styled.js file has been created!');
+
+		/* filter the file and return the tags */
+		tagsArray = findTagsInCurrentFile(wsPath, tagsArray);
+		console.log('returned tag array', tagsArray);
+		if (tagsArray <= 0) {
+			return vscode.window.showInformationMessage('No tags found, Please make sure to use <S. at the start of the tag');
+		}
+		const stringTagsArray = tagsArray.join("\n")
+		/* get the tags array */
+		if (fs.existsSync(wsPath + '/' + fileNameString)) {
+			await editStyledFile(wsPath, stringTagsArray);
+			return;
+		}
+
+		/* create the styled.js file */
+		await createStyledFile(wsPath, stringTagsArray);
+		return;
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-function checkIfOnValidFile(path) {
-	// need to check if the user activate the command on the right jsx file 
-	// search using regex the "<S." and make sure their is matching in the other file or something
+function findTagsInCurrentFile() {
+	if (vscode.window.activeTextEditor.document.fileName.includes(fileNameString)) {
+		return [];
+	}
+	const regex = /^(<S.)$/g;
+	const contents = fs.readFileSync(vscode.window.activeTextEditor.document.fileName, 'utf-8');
+	const arrFile = contents.split(/\r?\n/);
+	const filterByTag = arrFile.filter((item) => item.includes('<S.'));
+	const removeTheTag = filterByTag.map((item) => item.match(regex)); // THIS IS NOT WORKING -> NEED TO CUT THE WORD TO AFTER THE <S. and before the >
+	debugger;
+
+	if (fillteredArray.length > 0) {
+		return fillteredArray;
+	}
+	return [];
 }
 
-//PLAN
-//user will issue the command on the right file
-//the extantion command will create the style file for him 
-//a lot of use with regex to make sure the file have all the right properties in it
-// note: make sure it will support existing file ! (if the user already have styled.js with content we dont wanna delete it but replace or edit)
+/**
+ * @param wsPath -> the path of the current folder
+ * @param tagsArray -> the array containing the tags for creating the styled components
+ */
+async function createStyledFile(wsPath, tagsArray) {
+	const styledPath = vscode.Uri.file(wsPath + '/' + fileNameString); // get the full path for the styled
+	console.log(vscode.window.activeTextEditor.document.fileName);
+	wsedit.createFile(styledPath, { overwrite: false });
+	await vscode.workspace.applyEdit(wsedit);
+
+	const data = 
+	`
+	import styled from \'react-style\';
+
+	${tagsArray}
+
+	export default S {
+		bla bla
+	}
+	`;
+	// how to wrigt in a file ?
+	fs.writeFileSync(styledPath.fsPath, data, 'utf-8');
+	await vscode.workspace.applyEdit(wsedit);
+	vscode.window.showInformationMessage('Your styled.js file has been created!');
+}
+
+/**
+ * @param wsPath -> the path of the current folder
+ * @param tagsArray -> the array containing the tags for creating the styled components
+ */
+async function editStyledFile(wsPath, tagsArray) {
+	const styledPath = vscode.Uri.file(wsPath + '/' + fileNameString); // get the full path for the styled
+	wsedit.createFile(styledPath, { overwrite: false });
+	await vscode.workspace.applyEdit(wsedit);
+	const contents = fs.readFileSync(styledPath.fsPath, 'utf-8');
+	const arrFile = contents.split(/\r?\n/);
+}
+
+
 
 // this method is called when your extension is deactivated
 function deactivate() {}
