c6b94a215ff745b7f033167552c04260739259f4
DadonStyle <noamoni9@gmail.com>
2022-10-18 17:32:23 +0300

    add start of support for editing file

--- extension.js
+++ extension.js
@@ -4,97 +4,108 @@ const fs = require('fs');
 
 /* vars */
 const wsedit = new vscode.WorkspaceEdit();
-const fileNameString = 'styled.js'
+const styledConst = 'styled.js'
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	
 	let disposable = vscode.commands.registerCommand('react-style-component-helper.helloWorld', async function () { // this syntax is a template from vscode
 		/* varbs */
-		const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath; // gets the path of the first workspace folder
-		let tagsArray = [];
+		const styledFilePath = vscode.Uri.file(vscode.workspace.workspaceFolders[0].uri.fsPath + '/' + styledConst); // gets the path of the styled file
+		const currentFilePath = vscode.window.activeTextEditor.document.fileName; // gets the path of the current path
 
 		/* validate that the user have any file open while executing the command */
 		if (typeof vscode.window.activeTextEditor === typeof undefined) {
 			return vscode.window.showInformationMessage('Please enter the required file and try again');
 		}
 
-		/* filter the file and return the tags */
-		tagsArray = findTagsInCurrentFile(wsPath, tagsArray);
-		console.log('returned tag array', tagsArray);
-		if (tagsArray <= 0) {
-			return vscode.window.showInformationMessage('No tags found, Please make sure to use <S. at the start of the tag');
-		}
-		const stringTagsArray = tagsArray.join("\n")
-		/* get the tags array */
-		if (fs.existsSync(wsPath + '/' + fileNameString)) {
-			await editStyledFile(wsPath, stringTagsArray);
+		/* filter the file and return the tags and the number of lines*/
+		const tagsObject = findTagsInCurrentFile(currentFilePath, '<S.', '>');
+		if (tagsObject.relevantTags <= 0 || tagsObject <= 0) {
 			return;
 		}
-
+		/* get the tags array */
+		if (fs.existsSync(styledFilePath.fsPath)) {
+			const styledTagsObject = findTagsInCurrentFile(styledFilePath.fsPath, 'const', '=');
+			if (tagsObject.relevantTags <= 0 || styledTagsObject <= 0) {
+				return;
+			}
+			await editStyledFile(styledFilePath, styledTagsObject.relevantTags, styledTagsObject.numOflines); // makes the code more readable
+			return;
+		}
 		/* create the styled.js file */
-		await createStyledFile(wsPath, stringTagsArray);
+		await createStyledFile(styledFilePath, tagsObject.relevantTags);
 		return;
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-function findTagsInCurrentFile() {
-	if (vscode.window.activeTextEditor.document.fileName.includes(fileNameString)) {
+/**
+ * @param path -> the path for the relevant file
+ */
+function findTagsInCurrentFile(path, fromSymbol, toSymbol) {
+	if (vscode.window.activeTextEditor.document.fileName.includes(styledConst)) { // check the current open file
+		vscode.window.showErrorMessage('Please open the relevant file, not styled.js');
 		return [];
 	}
-	const regex = /^(<S.)$/g;
-	const contents = fs.readFileSync(vscode.window.activeTextEditor.document.fileName, 'utf-8');
-	const arrFile = contents.split(/\r?\n/);
-	const filterByTag = arrFile.filter((item) => item.includes('<S.'));
-	const removeTheTag = filterByTag.map((item) => item.match(regex)); // THIS IS NOT WORKING -> NEED TO CUT THE WORD TO AFTER THE <S. and before the >
-	debugger;
 
-	if (fillteredArray.length > 0) {
-		return fillteredArray;
+	const contents = fs.readFileSync(path, 'utf-8');
+	const arrFile = contents.split(/\r?\n/);
+	const numOflines = 20; // TODO
+	const filterByTag = arrFile.filter((item) => item.includes(fromSymbol));
+	const removeTheTag = filterByTag.map((item) => item.split(fromSymbol).pop().split(toSymbol)[0].trim());
+	const relevantTags =  Array.from(new Set(removeTheTag));
+
+	if (relevantTags.length > 0 && numOflines > 0) {
+		return { relevantTags, numOflines };
 	}
+	vscode.window.showErrorMessage('No tags found, Please make sure to use <S. at the start of the tag');
 	return [];
 }
 
 /**
- * @param wsPath -> the path of the current folder
+ * @param pathStyled -> the path of the *future* styled.js
  * @param tagsArray -> the array containing the tags for creating the styled components
  */
-async function createStyledFile(wsPath, tagsArray) {
-	const styledPath = vscode.Uri.file(wsPath + '/' + fileNameString); // get the full path for the styled
-	console.log(vscode.window.activeTextEditor.document.fileName);
-	wsedit.createFile(styledPath, { overwrite: false });
+async function createStyledFile(pathStyled, tagsArray) {
+	wsedit.createFile(pathStyled, { overwrite: false });
 	await vscode.workspace.applyEdit(wsedit);
 
-	const data = 
-	`
-	import styled from \'react-style\';
+	// data need to not contain tabs in this file to look good on the target file
+	const data =`
+import styled from \'react-style\';
+${tagsArray.map((item) => `\nconst ${item} = styled.div\`
+display: flex;
+\`;\n`).join('')}
+export default S = {
+${tagsArray.map((item) => `${item},\n`).join('')}
+};
+`;
 
-	${tagsArray}
-
-	export default S {
-		bla bla
-	}
-	`;
-	// how to wrigt in a file ?
-	fs.writeFileSync(styledPath.fsPath, data, 'utf-8');
-	await vscode.workspace.applyEdit(wsedit);
+	fs.writeFileSync(pathStyled.fsPath, data, 'utf-8'); // create the file
+	await vscode.workspace.applyEdit(wsedit); // finilazing the edit
 	vscode.window.showInformationMessage('Your styled.js file has been created!');
 }
 
 /**
- * @param wsPath -> the path of the current folder
+ * @param pathStyled -> the path of the *existing* styled.js
  * @param tagsArray -> the array containing the tags for creating the styled components
  */
-async function editStyledFile(wsPath, tagsArray) {
-	const styledPath = vscode.Uri.file(wsPath + '/' + fileNameString); // get the full path for the styled
-	wsedit.createFile(styledPath, { overwrite: false });
-	await vscode.workspace.applyEdit(wsedit);
-	const contents = fs.readFileSync(styledPath.fsPath, 'utf-8');
-	const arrFile = contents.split(/\r?\n/);
+// TODO: bc i cant know where to start and close the ``; (the user can have those in the mid of the component)
+// need to edit the new components after the end of their file, (the user will need to copy paste to the location)
+async function editStyledFile(pathStyled, tagsArray, numOfLines) {
+	// const existingTags = tagsArray.filter((item) => )
+
+	// data need to not contain tabs in this file to look good on the target file
+	const data =`
+
+`;
+
+	// fs.writeFileSync(pathStyled.fsPath, data, 'utf-8');
+	// await vscode.workspace.applyEdit(wsedit);
+	vscode.window.showInformationMessage('Your styled.js file has been edited!');
 }
 
 
