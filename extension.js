9f5bffd240e50150b3b6a4ce7afc37b54a21405a
DadonStyle <noamoni9@gmail.com>
2022-11-12 12:45:50 +0200

    bug fix, and change function name

--- extension.js
+++ extension.js
@@ -10,38 +10,33 @@ const styledConst = 'styled.js'
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('react-style-component-helper.helloWorld', async function () { // this syntax is a template from vscode
-		/* varbs */
-		const styledFilePath = vscode.Uri.file(vscode.workspace.workspaceFolders[0].uri.fsPath + '/' + styledConst); // gets the path of the styled file
-		const currentFilePath = vscode.window.activeTextEditor.document.fileName; // gets the path of the current path
-
+	let disposable = vscode.commands.registerCommand('react-style-component-helper.generateStyled', async function () { // this syntax is a template from vscode
 		/* validate that the user have any file open while executing the command */
 		if (typeof vscode.window.activeTextEditor === typeof undefined) {
 			return vscode.window.showInformationMessage('Please enter the required file and try again');
 		}
-
+		/* validate that the user is not on the styled js file */
+		if (vscode.window.activeTextEditor.document.fileName.includes(styledConst)) { // check the current open file
+			vscode.window.showErrorMessage('Please open the file to create from, not styled.js');
+			return [];
+		}
+		/* varbs */
+		const styledFilePath = vscode.Uri.file(vscode.workspace.workspaceFolders[0].uri.fsPath + '/' + styledConst); // gets the path of the styled file
+		const currentFilePath = vscode.window.activeTextEditor.document.fileName; // gets the path of the current path
 		/* filter the file and return the tags and the number of lines*/
 		const tagsObject = findTagsInCurrentFile(currentFilePath, '<S.', '>');
-		if (tagsObject.relevantTags <= 0 || tagsObject <= 0) {
+		if (!tagsObject.relevantTags || tagsObject.relevantTags.length <= 0) {
+			vscode.window.showErrorMessage('No tags found, Please make sure to use <S. at the start of the tag');
 			return;
 		}
-		/* get the tags array */
+		/* check if styled.js file already exist */
 		if (fs.existsSync(styledFilePath.fsPath)) {
+			let relevantTags = tagsObject.relevantTags;
 			const styledTagsObject = findTagsInCurrentFile(styledFilePath.fsPath, 'const', '=');
-			const duplicateMap = new Map(tagsObject.relevantTags.map((item) => [item, item]))
-			const relevantTags = [];
-			styledTagsObject.relevantTags.forEach((item
-				) => {
-					if(duplicateMap.get(item)){
-						return;
-					}
-					relevantTags.push(item);
-				});
-			// const relevantTags = Array.from(new Set(arr));
-			if (tagsObject.relevantTags <= 0 || styledTagsObject <= 0) {
-				return;
+			if (styledTagsObject.relevantTags && styledTagsObject.relevantTags.length > 0) {
+				relevantTags = handleDuplicate(styledTagsObject.relevantTags, tagsObject.relevantTags);
 			}
-			await editStyledFile(styledFilePath.fsPath, relevantTags, styledTagsObject.numOflines); // makes the code more readable
+			await editStyledFile(styledFilePath.fsPath, relevantTags, styledTagsObject.numOflines);
 			return;
 		}
 		/* create the styled.js file */
@@ -53,26 +48,38 @@ function activate(context) {
 }
 
 /**
+ * @param styledArr -> tags that been found in the styled.js file
+ * @param fileArr -> tags that been found in the file
+ */
+function handleDuplicate(styledArr, fileArr) {
+	const duplicateMap = new Map(styledArr.map((item) => [item, item]));
+	let returnedTags = [];
+	fileArr.forEach((item
+		) => {
+			if(duplicateMap.get(item)){
+				return;
+			}
+			returnedTags.push(item);
+		});
+	return returnedTags;
+}
+
+/**
  * @param path -> the path for the relevant file
+ * @param fromSymbol -> where to start cut
+ * @param toSymbol -> where to stop cut
  */
 function findTagsInCurrentFile(path, fromSymbol, toSymbol) {
-	if (vscode.window.activeTextEditor.document.fileName.includes(styledConst)) { // check the current open file
-		vscode.window.showErrorMessage('Please open the relevant file, not styled.js');
-		return [];
-	}
-
+	let relevantTags = [];
+	let numOflines = 1; // even if the file is empty the first line will be empty string so its 1
 	const contents = fs.readFileSync(path, 'utf-8');
 	const arrFile = contents.split(/\r?\n/);
-	const numOflines = arrFile.length; // TODO
+	numOflines = arrFile.length;
 	const filterByTag = arrFile.filter((item) => item.includes(fromSymbol));
 	const removeTheTag = filterByTag.map((item) => item.split(fromSymbol).pop().split(toSymbol)[0].trim());
-	const relevantTags =  Array.from(new Set(removeTheTag));
+	relevantTags =  Array.from(new Set(removeTheTag));
 
-	if (relevantTags.length > 0 && numOflines > 0) {
-		return { relevantTags, numOflines };
-	}
-	vscode.window.showErrorMessage('No tags found, Please make sure to use <S. at the start of the tag');
-	return [];
+	return { relevantTags, numOflines };
 }
 
 /**
@@ -85,7 +92,7 @@ async function createStyledFile(pathStyled, tagsArray) {
 
 	// data need to not contain tabs in this file to look good on the target file
 	const data =`
-import styled from \'react-style\';
+import styled from \'styled-components/macro\';
 ${tagsArray.map((item) => `\nconst ${item} = styled.div\`
 display: flex;
 \`;\n`).join('')}
@@ -102,17 +109,21 @@ ${tagsArray.map((item) => `${item},\n`).join('')}
 /**
  * @param pathStyled -> the path of the *existing* styled.js
  * @param tagsArray -> the array containing the tags for creating the styled components
+ * @param numOfLines -> number of lines in the file
  */
-// TODO: bc i cant know where to start and close the ``; (the user can have those in the mid of the component)
-// need to edit the new components after the end of their file, (the user will need to copy paste to the location)
+
 async function editStyledFile(pathStyled, tagsArray, numOfLines) {
 	const contents = fs.readFileSync(pathStyled, 'utf-8');
 	const arrFile = contents.split(/\r?\n/);
+	if (tagsArray.length < 1) {
+		vscode.window.showInformationMessage('No differences has been found');
+		return;
+	}
 
 	// data need to not contain tabs in this file to look good on the target file
 	const data =`
 ${arrFile.map((item) => `${item} \n`).join('')}
-
+${numOfLines > 1 ? '' : 'import styled from \'styled-components/macro\';'}
 ${tagsArray.map((item) => `\nconst ${item} = styled.div\`
 display: flex;
 \`;\n`).join('')}
