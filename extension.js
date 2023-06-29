6de7fbb2b2dae15520c06524836717a1efc9beec
DadonStyle <noamoni9@gmail.com>
2023-06-29 17:10:47 +0300

    change FS to remove sync as much as possible, fix path on MAC

--- extension.js
+++ extension.js
@@ -1,10 +1,11 @@
 /* imports */
 const vscode = require("vscode");
-const fs = require("fs");
+const fsSync = require("fs");
+const fs = require("fs").promises;
 
 /* vars */
-const wsedit = new vscode.WorkspaceEdit();
-const styledConst = "styled.js";
+// const wsedit = new vscode.WorkspaceEdit();
+const styledFileName = "styled.js";
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -15,89 +16,59 @@ function activate(context) {
     async function () {
       // this syntax is a template from vscode
       /* validate that the user have any file open while executing the command */
-      if (typeof vscode.window.activeTextEditor === typeof undefined) {
+      if (!vscode.window.activeTextEditor) {
         return vscode.window.showInformationMessage(
-          "Please enter the required file and try again"
+          "Please enter to the required file and try again"
         );
       }
-      /* validate that the user is not on the styled js file */
-      if (
-        vscode.window.activeTextEditor.document.fileName.includes(styledConst)
-      ) {
-        // check the current open file
+      // validate that the user is not on the styled js file
+      if (vscode.window.activeTextEditor.document.fileName.includes(styledFileName)) {
         vscode.window.showErrorMessage(
           "Please open the file to create from, not styled.js"
         );
         return [];
       }
-
       /* varbs */
-      const currentFileName = vscode.window.activeTextEditor.document.fileName
-        .split("\\")
-        .pop()
-        .split(".")[0]
-        .trim(); // gets the name to replace
-      const styledPath = vscode.window.activeTextEditor.document.fileName
-        .split("\\")
-        .reverse()
-        .join("/")
-        .replace(currentFileName, "styled")
-        .replace("jsx", "js")
-        .split("/")
-        .reverse()
-        .join("/"); // gets the path of the styled file
-      const styledFilePath = vscode.Uri.file(styledPath); // puts the path inside vscode editor (without we cant create files)
-      const currentFilePath = vscode.window.activeTextEditor.document.fileName
-        .split("\\")
-        .join("/"); // gets the path of the current path
-
-      const permissionsObj = getPermissions(currentFilePath);
-
-      const newPermissions = permissionsObj.ownerPermissions | 0o200;
-      fs.chmodSync(currentFilePath, newPermissions);
-
-      console.log("Write permissions granted");
-
-      await vscode.workspace.applyEdit(wsedit); // might be removable, make sure the workspace is open for edit
+      const currentFileName = vscode.window.activeTextEditor.document.fileName.split('/').pop(); // get current file name to replace
+      const currentFilePath = vscode.window.activeTextEditor.document.fileName; // get the full user path
+      const styledPath = currentFilePath.replace(currentFileName, styledFileName); // get the location of the styled path (or where to create it)
 
       /* filter the file and return the tags and the number of lines*/
-      const tagsObject = findTagsInCurrentFile(currentFilePath, "<S.", ">");
+      const tagsObject = await findTagsInCurrentFile(currentFilePath, "<S.", ">");
       if (!tagsObject.relevantTags || tagsObject.relevantTags.length <= 0) {
         vscode.window.showErrorMessage(
           "No tags found, Please make sure to use <S. at the start of the tag"
         );
         return;
       }
+
       /* check if styled.js file already exist */
-      if (fs.existsSync(styledFilePath.fsPath)) {
+      const isExist = fsSync.existsSync(styledPath);
+      if (isExist) {
         let relevantTags = tagsObject.relevantTags;
         const styledTagsObject = findTagsInCurrentFile(
-          styledFilePath.fsPath,
+          styledPath,
           "const",
           "="
         );
-        if (
-          styledTagsObject.relevantTags &&
-          styledTagsObject.relevantTags.length > 0
-        ) {
+        if (styledTagsObject.relevantTags && styledTagsObject.relevantTags.length > 0) {
           relevantTags = handleDuplicate(
             styledTagsObject.relevantTags,
             tagsObject.relevantTags
           );
         }
         await editStyledFile(
-          styledFilePath.fsPath,
+          styledPath,
           relevantTags,
           styledTagsObject.numOflines
         );
         return;
       }
       /* create the styled.js file */
-      await createStyledFile(styledFilePath, tagsObject.relevantTags);
+      await createStyledFile(styledPath, tagsObject.relevantTags);
       return;
-    }
+    },
   );
-
   context.subscriptions.push(disposable);
 }
 
@@ -123,16 +94,15 @@ function handleDuplicate(styledArr, fileArr) {
  * @param fromSymbol -> where to start cut
  * @param toSymbol -> where to stop cut
  */
-function findTagsInCurrentFile(path, fromSymbol, toSymbol) {
+async function findTagsInCurrentFile(path, fromSymbol, toSymbol) {
 	let relevantTags = [];
 	let numOflines = 1; // even if the file is empty the first line will be empty string so its 1
-	const contents = fs.readFileSync(path, 'utf-8');
-	const arrFile = contents.split(/\r?\n/);
+  const content = await readFile(path);
+	const arrFile = content.split(/\r?\n/);
 	numOflines = arrFile.length;
 	const filterByTag = arrFile.filter((item) => item.includes(fromSymbol));
 	const removeTheTag = filterByTag.map((item) => item.split(fromSymbol).pop().split(toSymbol)[0].trim());
 	relevantTags =  Array.from(new Set(removeTheTag));
-
 	return { relevantTags, numOflines };
 }
 
@@ -140,12 +110,9 @@ function findTagsInCurrentFile(path, fromSymbol, toSymbol) {
  * @param pathStyled -> the path of the *future* styled.js
  * @param tagsArray -> the array containing the tags for creating the styled components
  */
-async function createStyledFile(pathStyled, tagsArray) {
-	wsedit.createFile(pathStyled, { overwrite: false });
-	await vscode.workspace.applyEdit(wsedit);
-	// data need to not contain tabs in this file to look good on the target file
-	const data =`
-import styled from \'styled-components/macro\';
+async function createStyledFile(styledPath, tagsArray) {
+// data need to not contain tabs in this file to look good on the target file
+	const data =`import styled from \'styled-components/macro\';
 ${tagsArray.map((item) => `\nconst ${item} = styled.div\`
 display: flex;
 \`;\n`).join('')}
@@ -153,9 +120,24 @@ export default S = {
 ${tagsArray.map((item) => `${item},\n`).join('')}
 };
 `;
-	fs.writeFileSync(pathStyled.fsPath, data, 'utf-8'); // create the file
-	await vscode.workspace.applyEdit(wsedit); // finilazing the edit
-	vscode.window.showInformationMessage('Your styled.js file has been created!');
+try {
+  fs.writeFile(styledPath, data, 'utf-8'); // create the file
+  vscode.window.showInformationMessage('Your styled.js file has been created!');
+} catch (err) {
+  vscode.window.showErrorMessage(err.message);
+  return;
+}	
+}
+
+/** 
+ * @param path -> path to the file
+ */
+async function readFile(path) {
+  const res = await fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) { vscode.window.showErrorMessage(err.message); return; }
+    return data;
+  });
+  return res;
 }
 
 /**
@@ -163,18 +145,16 @@ ${tagsArray.map((item) => `${item},\n`).join('')}
  * @param tagsArray -> the array containing the tags for creating the styled components
  * @param numOfLines -> number of lines in the file
  */
-
 async function editStyledFile(pathStyled, tagsArray, numOfLines) {
-	const contents = fs.readFileSync(pathStyled, 'utf-8');
-	const arrFile = contents.split(/\r?\n/);
+	const content = await readFile(pathStyled);
+	const arrFile = content.split(/\r?\n/);
 	if (tagsArray.length < 1) {
 		vscode.window.showInformationMessage('No differences has been found');
 		return;
 	}
 
 	// data need to not contain tabs in this file to look good on the target file
-	const data =`
-${arrFile.map((item) => `${item} \n`).join('')}
+	const data =`${arrFile.map((item) => `${item} \n`).join('')}
 ${numOfLines > 1 ? '' : 'import styled from \'styled-components/macro\';'}
 ${tagsArray.map((item) => `\nconst ${item} = styled.div\`
 display: flex;
@@ -183,18 +163,9 @@ export default S = {
 ${tagsArray.map((item) => `${item},\n`).join('')}
 };
 
-`;	await vscode.workspace.applyEdit(wsedit); // finilazing the edit
-	fs.writeFileSync(pathStyled, data, 'utf-8'); // create the file
-	await vscode.workspace.applyEdit(wsedit); // finilazing the edit
+`;	
+	fs.writeFile(pathStyled, data, 'utf-8'); // create the file
 	vscode.window.showInformationMessage('Your styled.js file has been edited!');
-}
-
-function getPermissions(filepath) {
-  const stats = fs.statSync(filepath);
-  const ownerPermissions = stats.mode & 0o700;
-  const groupPermissions = stats.mode & 0o070;
-  const otherPermissions = stats.mode & 0o007;
-  return ownerPermissions, groupPermissions, otherPermissions;
 }
 
 // this method is called when your extension is deactivated
